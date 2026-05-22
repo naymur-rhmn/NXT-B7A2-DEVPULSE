942e06a1cc0de3aa8521e083d367931e8b3a3f7a
Naymur Rahman <naymurmn@gmail.com>
2026-05-22 22:04:48 +0600

    setup database initialization

---
+++ src/db/index.ts
@@ -0,0 +1,25 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+    connectionString: config.connection_url
+});
+
+export const initDB = async () => {
+    try {
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS users(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL,
+                    email VARCHAR(50) UNIQUE NOT NULL,
+                    password TEXT NOT NULL,
+                    role VARCHAR(20) DEFAULT 'contributor' CHECK(role IN ('contributor', 'maintainer')),
+                    created_at TIMESTAMP DEFAULT NOW(),
+                    updated_at TIMESTAMP DEFAULT NOW()
+                )
+            `)
+        console.log("Database connected successfully!");
+    } catch (error) {
+        console.error("Failed to connect database:", error);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import app from "./app";
 import config from "./config"; 
+import { initDB } from "./db";
 
 const main = () => { 
-    
+    initDB();
     app.listen(config.port, () => {
         console.log("The server is lestening on port: ", config.port)
     })
